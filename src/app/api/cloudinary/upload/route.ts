a80a18e1914b6bb68b689bf2ccdd5425d6a83c79
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-04-15 17:56:55 -0400

    fixed

--- src/app/api/cloudinary/upload/route.ts
+++ src/app/api/cloudinary/upload/route.ts
@@ -10,13 +10,13 @@ cloudinary.config({
 
 export async function POST(req: Request) {
 
-    const image = req.body.get('image');
+    // const image = req.body.get('image');
 
-    if (!image) {
-        return NextResponse.error(new Error('No se proporcionó ninguna imagen'), { status: 400 });
-    }
+    // if (!image) {
+    //     return NextResponse.error(new Error('No se proporcionó ninguna imagen'), { status: 400 });
+    // }
 
-    const results = await cloudinary.uploader.upload(image);
+    // const results = await cloudinary.uploader.upload(image);
 
-    return NextResponse.json({ ...results, status: 200 })
+    // return NextResponse.json({ ...results, status: 200 })
 }
