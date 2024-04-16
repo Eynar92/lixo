6a34bdb56bd07caefd2aa2ae7b75d901495680e3
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-04-16 00:39:17 -0400

    upload photo by input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const api_key = process.env.CLOUDINARY_API_KEY!;
   const api_secret = process.env.CLOUDINARY_API_SECRET!;
 
-  const [imageSrc, setImageSrc] = useState<string | null>();
+  const [imageSrc, setImageSrc] = useState<File | null>();
   const [url, setUrl] = useState('');
   const webcamRef = useRef<Webcam>(null);
 
@@ -35,27 +35,11 @@ export default function Home() {
 
       console.log(cloud_name);
 
-
-      // for (const pair of formData.entries()) {
-      //   console.log(pair[0], pair[1]);
-      // }
-
       try {
         const response = await fetch(`https://api.cloudinary.com/v1_1/dzib8jnnl/image/upload`, {
           method: 'POST',
           body: formData
         });
-
-        // const response = await axios.post(`https://api.cloudinary.com/v1_1/dzib8jnnl/image/upload`, formData, {
-        //   headers: {
-        //     'Content-Type': 'multipart/form-data', // Asegura que el tipo de contenido sea multipart/form-data
-        //   },
-        //   params: {
-        //     upload_preset: upload_preset,
-        //     public_id: public_id,
-        //     api_key: api_key,
-        //   },
-        // });
 
         if (!response.ok) {
           throw new Error(`Error: ${response.status} - ${response.statusText}`);
@@ -68,81 +52,32 @@ export default function Home() {
         console.error('Upload failed:', error);
       }
     })();
+    console.log(imageSrc);
 
   }, [imageSrc]);
 
-  const handleOnCapture = () => {
-    const image = webcamRef.current?.getScreenshot();
-    image && setImageSrc(image);
-  }
 
-  const handleResetImage = () => {
-    setImageSrc(null)
-  }
-
-  const handleSaveImage = async () => {
-    const response = await fetch('/api/cloudinary/upload', {
-      method: 'GET',
-      // body: JSON.stringify({
-      //   imageSrc
-      // })
-    })
-
-    console.log(response.json);
-
-  }
 
   return (
     <main className="py-4 layout h-dvh">
       <Container>
-        {/* <input
+        <input
           type="file"
           accept="image/*"
-          onChange={(e) => setImageSrc(e.target.files[0])}
-        /> */}
-        <div className="w-full sm:w-[500px] mt-0 mx-auto mb-8">
-          <div className="relative aspect-auto bg-black">
-            <button
-              className='absolute top-0 right-0 bg-gray-100/50 size-6 text-center rounded-full'
-              onClick={handleResetImage}
-            >
-              X
-            </button>
-            <div className="m-auto">
-              {
-                imageSrc ? (
-                  <img src={imageSrc} alt="Image" className="block w-full h-auto" />
-                ) : (
-                  <Webcam
-                    ref={webcamRef}
-                    screenshotQuality={1}
-                    videoConstraints={videoConstraints}
-                  />
-                )
-              }
-            </div>
-          </div>
-        </div>
+          capture='user'
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+            if (file) {
+              setImageSrc(file);
+            }
+          }}
+        />
+        {
+          imageSrc && (
+            <img src={URL.createObjectURL(imageSrc)} alt='Image' className='max-h-52 w-auto' />
+          )
+        }
 
-        <div className="w-full sm:w-[500px] mx-auto">
-          <ul className="flex justify-between">
-            <li>
-              <Button
-                onClick={handleOnCapture}
-              >
-                Tomar Foto
-              </Button>
-            </li>
-            <li>
-              <Button
-                className="bg-green-100 text-green-700 hover:bg-green-200"
-                onClick={handleSaveImage}
-              >
-                Guardar Foto
-              </Button>
-            </li>
-          </ul>
-        </div>
       </Container>
     </main>
   );
